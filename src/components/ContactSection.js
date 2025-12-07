c7a0bb1116384237bf4d25d7ed3e7fc1a16d9b44
programmer98177 <programmer98177@gmail.com>
2025-12-07 19:44:27 +0545

    Update ContactSection.js

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -21,7 +21,7 @@ export default function ContactSection() {
                         READY TO TAKE YOUR BRAND TO THE NEXT LEVEL?
                     </p>
                     <p className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold text-black leading-tight mt-2">
-                        CONNECT WITH VyralXA AND LET'S GROW TOGETHER.
+                        CONNECT WITH VyralX AND LET'S GROW TOGETHER.
                     </p>
                 </div>
 
@@ -29,4 +29,5 @@ export default function ContactSection() {
         </section>
     );
 }
+
 
